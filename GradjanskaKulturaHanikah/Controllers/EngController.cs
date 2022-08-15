981f9eabe696d08d7d9f9a9c54b04906b9924f19
aalialzaya1 <aalialzaya1@etf.unsa.ba>
2022-08-15 14:00:22 +0200

    Add project files.

---
+++ GradjanskaKulturaHanikah/Controllers/EngController.cs
@@ -0,0 +1,37 @@
+﻿using GradjanskaKulturaHanikah.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GradjanskaKulturaHanikah.Controllers
+{
+    public class EngController : Controller
+    {
+        private readonly ILogger<EngController> _logger;
+
+        public EngController(ILogger<EngController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult EngIndex()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
